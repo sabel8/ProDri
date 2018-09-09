04fd9cd19e7bca4d67316b76e0fb7d6d455e1e66
Sárándi Ábel <sarandi.abel@gmail.com>
2018-09-09 14:33:10 +0200

    dragging from node
    nodes now stores own unique ID;

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ window.onload = function() {
 	var width = window.innerWidth*0.8,
 		height = 500;
 	var nodes = new Array();
-	var disableMove=true;
-	var mouseOverCircle = false;
-	nodes.push(new Node("példa 1",300,200));
-	nodes.push(new Node("példa 2",100,300));
+	nodes.push(new Node(0,"példa 1",300,200));
+	nodes.push(new Node(1,"példa 2",100,300));
 
 
 	d3.select("body").append("h1").text("Üdv!");
@@ -29,6 +27,8 @@ window.onload = function() {
 		d3.select("#graph").attr("transform", d3.event.transform);
 	}
 
+	var mousePos;
+
 	function redraw(){
 		var g = d3.select("#graph").selectAll("g")
 			.data(nodes)
@@ -38,17 +38,25 @@ window.onload = function() {
 				return "translate("+d.x+","+d.y+")"
 			})
 			.on("mouseenter", function(){
-				mouseOverCircle = true;
 				d3.select(this).classed("hover",true);
 			})
 			.on("click",function(d,i){
 				alert("Here will be the info of the node/task."
 					+"\nThis node has the text \""+d.txt+"\" on it."
-					+"\nIts ID is "+i+".")})
+					+"\nIts ID is "+d.ID+".")})
 			.on("mouseleave", function(){
-				mouseOverCircle = false;
 				d3.select(this).classed("hover",false);
-			});
+			})
+			.on("mousedown", function(d){
+				mousePos = d3.mouse(this);
+				d3.select("#graph").append("path")
+		        .attr('d', 'M' + d.x + ',' + d.y + 'L' + d.x + ',' + d.y);
+			})
+			.call(d3.drag()
+				.on("start", dragstarted)
+	            .on("drag", dragged)
+	            .on("end", dragend));
+
 		g.append("circle")
 			.attr("r", 40)
 			.attr("fill","lightblue")
@@ -59,24 +67,50 @@ window.onload = function() {
 		});
 	}
 
+	function dragstarted(d) {
+		event.stopPropagation();
+		d3.select("#graph").append("path").attr("id","new");
+		let e = d3.event;/*
+		console.log(mousePos);
+		console.log("event:  "+e.x+","+e.y);
+		console.log("circle: "+d.x+","+d.y);*/
+	}
+	
+	function dragged(d){
+		let e = d3.event;
+		let mouseX = e.x+mousePos[0];
+		let mouseY = e.y+mousePos[1];
+		d3.select("#new")
+			.attr("d","M"+d.x+","+d.y+"L"+mouseX+","+mouseY+"Z")
+			.attr("stroke","black")
+			.attr("stroke-width","5");
+		//redraw();
+	}
+	
+	function dragend(d) {
+
+	}
+
+	
 
 	function createNode(){
 		if (d3.event.shiftKey) {
-
 			let event = d3.mouse(d3.select("#graph").node());
 			let txt = prompt("Please enter the title of the task", "Példa egy");
 			if (txt===null){return;}
-			let a = new Node(txt,event[0],event[1]);
-			nodes.push(a);
-			redraw();
-		};
+			nodes.push(new Node(nodes.length,txt,event[0],event[1]));
+		}
+		redraw();
 	}
+
+
 	redraw();
 };
 
 
 //constructor of the Node element
-function Node(txt, x, y) {
+function Node(ID, txt, x, y) {
+	this.ID = ID;
 	this.txt = txt;
 	this.x = x;
 	this.y = y;
